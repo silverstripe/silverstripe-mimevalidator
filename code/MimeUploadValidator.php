c6c1e200712f8d1cdce99367356a62b503a104ee
Ishan Jayamanne <ishanjayamanne@Mac-C02J43Z7DKQ4.wgtn.silverstripe.com>
2017-03-27 10:24:31 +1300

    fix for multiple mime type in yml, when i adding multiple mime to yml file for a single extention e.g - svg, its not adding to  array.

--- code/MimeUploadValidator.php
+++ code/MimeUploadValidator.php
@@ -88,11 +88,11 @@ class MimeUploadValidator extends Upload_Validator
         // Get the mime types and their variations from mimevalidator
         $knownMimes = Config::inst()->get(get_class($this), 'MimeTypes');
         if (isset($knownMimes[$extension])) {
-            if (is_array($knownMimes[$extension])) {
-                $expectedMimes += $knownMimes[$extension];
-            } else {
-                $expectedMimes[] = $knownMimes[$extension];
-            }
+          foreach($knownMimes[$extension] as $mime){
+              if(!in_array($mime,$expectedMimes)){
+                  $expectedMimes[] = $mime;
+              }
+          }
         }
         return $expectedMimes;
     }
