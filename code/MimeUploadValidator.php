b1460de342c622f5c9483f262657f95ea787de59
Ishan Jayamanne <ishanjayamanne@Mac-C02J43Z7DKQ4.wgtn.silverstripe.com>
2017-03-27 13:05:44 +1300

    code refactor

--- code/MimeUploadValidator.php
+++ code/MimeUploadValidator.php
@@ -88,8 +88,8 @@ class MimeUploadValidator extends Upload_Validator
         // Get the mime types and their variations from mimevalidator
         $knownMimes = Config::inst()->get(get_class($this), 'MimeTypes');
         if (isset($knownMimes[$extension])) {
-          $knownMimes[$extension] = array() $knownMimes[$extension];
-          foreach($knownMimes[$extension] as $mime){
+          $mimes = array() $knownMimes[$extension];
+          foreach($mimes as $mime){
               if(!in_array($mime,$expectedMimes)){
                   $expectedMimes[] = $mime;
               }
